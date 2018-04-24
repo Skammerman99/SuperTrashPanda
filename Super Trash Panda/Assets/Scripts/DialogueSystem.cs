2351ef93428aebcda82e5069c522a2b99b15f656
Wayne <weiyanz@uci.edu>
2018-04-24 13:44:26 -0700

    Basic Dialogue System
    Still need to be modified after we know how the dialogue will be triggered.

---
+++ Super Trash Panda/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour {
+    public GameObject textBoxBG;
+    public Text textBox;
+
+    public Dialogue[] dialogues;
+
+    public void StratDialogue()
+    {
+        StartCoroutine(Text());
+    }
+
+    public IEnumerator Text()
+    {
+        ShowTextBox();
+        foreach(Dialogue dialogue in dialogues)
+        {
+            if(dialogue.speakerName == "")
+            {
+                yield return (ChangeText(dialogue.sentence, 0, 0.01f));
+            }
+            else
+            {
+                yield return (ChangeText(dialogue.speakerName +":\n"+dialogue.sentence, dialogue.speakerName.Length + 1, 0.01f));
+            }
+            
+        }
+        CloseTextBox();
+    }
+
+    IEnumerator ChangeText(string text, int starting, float interval)
+    {
+        for(int x = starting; x<= text.Length; ++x)
+        {
+            textBox.text = text.Substring(0, x);
+            yield return new WaitForSeconds(interval);
+        }
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
+    }
+
+    public void ShowTextBox()
+    {
+        textBoxBG.SetActive(true);
+    }
+
+    public void CloseTextBox()
+    {
+        textBoxBG.SetActive(false);
+    }
+
+}
