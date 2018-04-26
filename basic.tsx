8772e6b618947adc3a39b9b9056422c3e1888cca
rvdang <rvdang98@gmail.com>
2018-04-25 18:23:20 -0700

    fixed collision clipping and changed player sprite

--- basic.tsx
+++ basic.tsx
@@ -12,88 +12,72 @@
   <terrain name="carpet bottom" tile="17"/>
  </terraintypes>
  <tile id="0">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="1">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="2">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="3">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="4">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="5">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="6">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="7">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="9" terrain="7,7,6,6"/>
  <tile id="13">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="14">
   <objectgroup draworder="index"/>
  </tile>
  <tile id="15">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="17" terrain="6,6,7,7"/>
  <tile id="21">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="23">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="24">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="14.03" y="0.5">
+    <polyline points="0,0 -13.4848,-0.0584231 -13.4848,13.5541"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="25">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="1.40599" y="0.5">
+    <polyline points="0,0 12.9324,0"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="26">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="1.5" y="0.5">
+    <polyline points="0,0 14.041,0.059061 14.1013,13.9384"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="27">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="29">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="30">
   <objectgroup draworder="index">
@@ -106,25 +90,27 @@
   </objectgroup>
  </tile>
  <tile id="32">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="1.53381">
+    <polyline points="0,0 0,12.9096"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="34">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="2" x="15.5" y="1.59772">
+    <polyline points="0,0 0,12.8602"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="35">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="36">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="37">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="38">
   <objectgroup draworder="index">
@@ -132,65 +118,57 @@
   </objectgroup>
  </tile>
  <tile id="40">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="2.03">
+    <polyline points="0,0 0,13.3635 13.4848,13.4208"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="41">
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="1.53381" y="15.5">
+    <polyline points="0,0 12.8457,0"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="42">
   <properties>
    <property name="unity:scale" type="float" value="0.0625"/>
   </properties>
-  <objectgroup draworder="index"/>
+  <objectgroup draworder="index">
+   <object id="1" x="15.5" y="2.1">
+    <polyline points="0,0 0,13.4031 -13.4795,13.4381"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="47">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="58">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="59">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="70">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="71">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="76">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="77">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="78">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="79">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
  <tile id="99">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <objectgroup draworder="index"/>
  </tile>
 </tileset>
--- collision.tsx
+++ collision.tsx
@@ -1,9 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="collision" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="../../../Desktop/Game/red-collision-box.png" width="16" height="16"/>
+ <image source="Super Trash Panda/Assets/Tiled2Unity/Textures/red-collision-box.png" width="16" height="16"/>
  <tile id="0">
   <objectgroup draworder="index">
-   <object id="1" x="1" y="1" width="14" height="14"/>
+   <object id="3" x="1.49982" y="1.49989">
+    <polyline points="0,0 13.0144,-0.00911493 13.0144,13.007 -0.0182659,12.9979 -0.0182659,0.00911493 -0.00913293,0 -0.00913293,-0.00911493 13.0236,-0.00911493 13.0236,13.007"/>
+   </object>
   </objectgroup>
  </tile>
 </tileset>
